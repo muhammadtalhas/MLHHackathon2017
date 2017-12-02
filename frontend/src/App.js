7368819cc500577fbb1b86ab586d0782c1342466
Brandon <veryyes10@yahoo.com>
2017-12-02 14:16:59 -0500

    Front end stuff

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Banner from './banner.js';
+import './style.css';
+
+class App extends Component{
+	render(){
+		return(
+			<div>
+				<Banner title="Issunews"/>
+				Stupid Bitch
+			</div>
+		);
+	}
+}
+
+export default App;
