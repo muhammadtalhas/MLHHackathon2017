ca7b7e0b285951d3917fa25c0f07ee5c2be46255
Talha <muhammadtalhas@yahoo.com>
2017-12-02 14:12:49 -0500

    Yo registration works

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@
 var express = require('express');
 var bodyParser = require('body-parser')
 var app = express();
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
 var dbConnector = require('./dbConnector/dbConnector.js');
 var dbConfig = require('./dbConnector/dbConfig.js');
 
 
-app.use(bodyParser.urlencoded({extended: true}));
 
 var db;
 dbConfig.mongoClient.connect(dbConfig.mongoUrl, function (err, database) {
@@ -23,6 +25,37 @@ const BASE_URL = '/api';
 app.get(BASE_URL, function (req, res) {
     res.send('Sup BITCHES')
 });
+
+app.post(BASE_URL + '/register', function(req, res){
+    var data = {
+        username: String(req.body.username).trim().toLowerCase(),
+        email: req.body.email,
+        password: req.body.password,
+        street_name: req.body.street_name,
+        city:req.body.city,
+        state:req.body.state,
+        zip:req.body.zip
+    };
+    console.log(data)
+    if(data.username
+        && data.email
+        && data.password
+        && data.street_name
+        && data.city
+        && data.state
+        && data.zip){
+
+        dbConnector.user.insertUser(db, data, function(err, response){
+            if(err){
+                res.send(err);
+            }else{
+                res.send(response)
+            }
+        })
+    }else{
+        res.send("Some data was missing")
+    }
+})
 
 app.listen(3000, function () {
     console.log('Listening on :3000')
