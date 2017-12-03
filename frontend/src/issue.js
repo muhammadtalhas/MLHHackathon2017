a7d4a7933211241b7b7dc55b66fd338fdf5b6f74
Talha <muhammadtalhas@yahoo.com>
2017-12-02 20:39:44 -0500

    BOIZ

--- frontend/src/issue.js
+++ frontend/src/issue.js
@@ -5,39 +5,82 @@ import Footer from './footer.js';
 import './style.css';
 
 class Issue extends Component{
-	render(){
-		return(
-			<div className="Issue">
-				<Banner title="Issunews" text="Net Neutrality" />
-				<Login />
-				<div className="content">
-					<form>
-					<img src={this.props.image} alt=""/>
-					<div>
-						<label>Email</label>
-						<br/>
-						<input type="text" name="email" value={this.props.userData?this.props.userData.email:""}/>
-					</div>
+    constructor(props) {
+        super(props);
+        this.state = {
+            userData: null
+        }
+    }
 
-					<div>
-					<label>Message</label>
-					<br/>
-					<textarea name="message" rows="6" cols="100">
+	activeIssue = null;
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.userData) {
+            this.setState({userData: nextProps.userData});
+        }
+
+        if (nextProps.issues) {
+            this.setState({issues: nextProps.issues});
+        }
+    }
+    componentDidMount(){
+    	console.log("mounted")
+    	this.activeIssue = this.getCurrentIssue(this.getIssueId())
+	}
+    getIssueId(){
+        var url = window.location.pathname;
+        var urlsplit = url.split("/").slice(-1)[0]
+		return urlsplit;
+	}
+
+	getCurrentIssue(issue_id){
+    	console.log(issue_id)
+		console.log(this.state.issues)
+    	for(var issue in this.state.issues){
+    		console.log(issue)
+    		if(issue._id === issue_id){
+
+    			return issue
+			}
+		}
+		return null;
+	}
+
+	render(){
+		if(this.getIssueId() === "5a2342bd10b3cc037ba01fe3") {
+            return (
+				<div className="Issue">
+					<Banner title="Issunews" text="Net Neutrality"/>
+					<Login onClick={this.props.onClick} userData={this.state.userData}/>
+					<div className="content">
+						<form>
+							<img src={this.props.image} alt=""/>
+							<div>
+								<label>Email</label>
+								<br/>
+								<input type="text" name="email"
+									   value={this.props.userData ? this.props.userData.email : ""}/>
+							</div>
+
+							<div>
+								<label>Message</label>
+								<br/>
+								<textarea name="message" rows="6" cols="100">
 						Net neutrality is the principle that Internet service providers must treat all data on the Internet the same, and not discriminate or charge differently by user, content, website, platform, application, type of attached equipment, or method of communication.For instance, under these principles, internet service providers are unable to intentionally block, slow down or charge money for specific websites and online content.
 					</textarea>
+							</div>
+
+							<p>
+								By clicking the "I Support" button, you agree to send the above to your constituents.
+							</p>
+							<ConstituentView userData={this.props.userData}/>
+							<br/>
+							<button type="submit" disabled={!this.props.userData}>I Support</button>
+						</form>
 					</div>
-	
-					<p>
-						By clicking the "I Support" button, you agree to send the above to your constituents.
-					</p>
-						<ConstituentView userData={this.props.userData} />	
-						<br/>
-						<button type="submit" disabled={!this.props.userData}>I Support</button>
-					</form>	
+					<Footer/>
 				</div>
-				<Footer />
-			</div>
-		);
+            );
+        }
 		
 	}
 }
--- frontend/src/issue_list.js
+++ frontend/src/issue_list.js
@@ -1,37 +1,41 @@
 import React, {Component} from 'react'
+import { Link } from 'react-router-dom'
+class IssueList extends Component {
+    render() {
+        if (this.props.issues) {
+            let issue = this.props.issues.map(function (item, i) {
+                //var link = `Issues:${item._id}`;
+                return (
+                    <Link to={'/Issue/'+item._id}>
+                        <div>
+                            <Issue key={item._id} title={item.title} description={item.description}/>
+                        </div>
+                    </Link>
+                );
+            });
 
-class IssueList extends Component{
-	render(){
-		if(this.props.issues){
-			let issue = this.props.issues.map(function(item, i){
-			return(
-				<div>	
-					<Issue key={item._id} title={item.title} description={item.description} />
-				</div>
-			);
-});
-			return(
-				<div>
-					{issue}
-				</div>
-			);
-		}else{
-			return(<div>No Issues!</div>);
-		}
-	}
+            return (
+                <div>
+                    {issue}
+                </div>
+            );
+        } else {
+            return (<div>No Issues!</div>);
+        }
+    }
 }
 
-class Issue extends Component{
-	render(){
-		return(
-			<div className="IssuePreview">
-				<h3>{this.props.title}</h3>
-				<p>
-					{this.props.description}
-				</p>
-			</div>
-		)
-	}
+class Issue extends Component {
+    render() {
+        return (
+            <div className="IssuePreview">
+                <h3>{this.props.title}</h3>
+                <p>
+                    {this.props.description}
+                </p>
+            </div>
+        )
+    }
 }
 
 export default IssueList;
